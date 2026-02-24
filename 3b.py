98f19309cdf70a55a8d208a257c3bc47671c9df8
Prathvik S <prathviks05@gmail.com>
2026-02-24 04:59:42 -0800

    Add files via upload

---
+++ 3b.py
@@ -0,0 +1,35 @@
+first_name = "prathvik"
+last_name = "S"
+
+
+full_name = first_name + "  " +last_name
+
+
+print(full_name)
+
+
+#repetition
+message = "this is Warning!"
+print(message*10)
+print(message.upper())
+print(message.lower())
+print(message.strip()*2)
+a =(message.replace("warning","error"))
+print(a)
+
+name = '''Prathvik said "hello"
+darshan said "hy"'''
+print(name)
+
+print(len(message))  #length
+
+name = "Prathvik"
+print(name[4]) # index = position-1
+print(name[2:6])
+print(name[:4])
+print(name[2:])
+print(name[-2])
+print(name[::2])
+
+s="Prathvik \tis a good boy"
+print(s)
