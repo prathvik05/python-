f91087b0b529ea0769a296f3c840a19d47988657
Prathvik S <prathviks05@gmail.com>
2026-02-24 21:04:19 -0800

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,18 @@
+#operators
+a = 10
+a += 100 #a = a+ 100 shortform
+print(a)
+
+a = 10
+b = 20
+print(a != b)
+
+print(not(False))
+print(1>2 and 2>1)
+
+s = "Prathvik"
+s2 ="prathviks"
+
+//print(("r" in s)  or ("r" in s2))
+
+print(not("k" in s))
