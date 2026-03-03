289b02ef5231c2fafb4f343f3bcd3d6e07396a51
Prathvik S <prathviks05@gmail.com>
2026-03-03 21:44:43 +0530

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,40 @@
+print("Hello good morning!")
+print("Hello good morning!")
+print("Hello good morning!")
+
+
+a,b = 1,2
+print(a+b)
+
+def greet():
+    print("Hello,Good morning!")
+    
+greet()
+greet()
+
+def marriage(boy,girl):
+    print(f"Boy is{boy}")
+    print(f"Girl is {girl}")
+    print(f"{boy} married {girl}")
+    
+marriage("chandan","sneha")#positionAl arguement
+marriage(boy="Narendra",girl="chandana")#keyword arguement
+
+def tables (num):
+    for i in range(1,11):
+        print(f"2X{i}={2*i}")
+    
+    
+tables(5)
+tables(6)
+tables(7)
+
+
+def  func():
+    x = "chandan"
+    print("Hello world")
+    print(y)
+    
+y= "darshan"
+
+print(y)
